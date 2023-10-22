d5ecb7dbd6b90c43a46db4bca02c513136558d67
maximemeshev <lemeshmah@gmail.com>
2023-10-22 18:09:26 +0300

    myfilter

--- index.js
+++ index.js
@@ -1,62 +1,12 @@
-let obj = {
-    from: 2,
-    to: '',
-  };
-
-  obj[Symbol.iterator] = function () {
-    if (typeof this.from !== "number" || typeof this.to !== "number") {
-      throw new Error("Error: 'from' or 'to' is not a number");
-    } else if (this.from >= this.to) {
-      throw new Error("Error: 'from' must be less than 'to'");
-    } else {
-      return {
-        current: this.from,
-        last: this.to,
-        next() {
-          if (this.current <= this.last) {
-            return { done: false, value: this.current++ };
-          } else {
-            return { done: true };
-          }
-        },
-      };
-    }
-  };
-
-  for (let key of obj) {
-    console.log(key);
+Array.prototype.myFilter = function (callback, thisArg) {
+  if (typeof callback !== "function") {
+    throw new TypeError("Callback must be a function");
   }
-
-function getPersons(name, age) {
-  // Использование классов
-  class Person {
-    constructor(name, age) {
-      this.name = name;
-      this.age = age;
+  let arr = [];
+  for (i = 0; i < this.length; i++) {
+    if (callback.call(thisArg, this[i], i, this)) {
+      arr.push(this[i]);
     }
   }
-  const john25 = new Person("John", 25);
-
-  // Использование функции-конструктора.
-  function Person2(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  const person = new Person2("John", 25);
-  // Просто задать свойства
-  const personObj = {};
-  personObj.name = "John";
-  personObj.age = 25;
-
-  return [
-    { name, age },
-    Object.assign({}, { name, age }),
-    new Object({ name, age }),
-    Object.create({ name, age }),
-    john25,
-    person,
-    personObj,
-  ];
-}
-
-console.log(getPersons("John", 25));
+  return arr;
+};
